3ac2b62ace7cb5f9f2bd4cfc88e14658f5e59ef9
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-05-06 19:07:01 +0200

    Implement basic set method

--- hash-map.rb
+++ hash-map.rb
@@ -1,8 +1,11 @@
 # raise IndexError if index.negative? || index >= @buckets.length
+require '../linked_lists_TOP/linked_lists'
 
 class HashMap
+  attr_accessor :buckets
+
   def initialize
-    @buckets = Array.new(16, Array.new)
+    @buckets = Array.new(16)
   end
 
   def hash(key)
@@ -10,20 +13,52 @@ class HashMap
     prime = 23
 
     key.each_char { |char| hash_code = prime * hash_code + char.ord }
-
-    hash_code
+    hash_code % @buckets.length
   end
 
   def set(key, value)
-    index = hash(key) % @buckets.length
+    index = hash(key)
     if @buckets[index].nil?
-      @buckets[index] = value
+      @buckets[index] = LinkedList.new
+      @buckets[index].append([key, value])
+    elsif node = find_key_in_linked_list(key, @buckets[index].head)
+      node.value[1] = value
     else
-      expand
+      @buckets[index].append([key, value])
+      # p @bucket[index]
     end
+  end
+
+  def has?(key)
+    @buckets.each do |bucket|
+      return true if find_key_in_linked_list(key, bucket.head)
+    end
+    false
+  end
+
+  def find_key_in_linked_list(key, current_node)
+    return current_node if current_node.value[0] == key
+    return nil if current_node.next_node == nil
+
+    current_node = current_node.next_node
+    find_key_in_linked_list(key, current_node)
+  end
+
+  def get(key)
+    index = hash(key)
+    @buckets[index]
   end
 end
 
 map = HashMap.new
+map.set('john', 'old value')
+map.set('mark', 'peak time')
+map.set('jim', 'bro')
+map.set('john', 'new value')
+map.set('nelson', 'amigo')
 
-puts map.buckets
+map.buckets.each_with_index do |bucket, index|
+  p "In bucket #{index + 1} we have #{bucket}"
+end
+keys = ['john', 'jack', 'kevin', 'marco', 'maxim', 'christian', 'johny', 'sara', 'nelson']
+
