900ae5420262145f5606f79d5852395ed29919bd
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-05-07 11:12:41 +0200

    Implement length

--- hash-map.rb
+++ hash-map.rb
@@ -46,6 +46,15 @@ class HashMap
     remove_key_in_linked_list(key, @buckets[index])
   end
 
+  def length
+    count = 0
+    @buckets.each do |bucket|
+      next if bucket.nil?
+      count += linked_list_length(bucket)
+    end
+    count
+  end
+
   def find_node_in_hash(key)
     index = hash(key)
     return if @buckets[index].nil?
@@ -53,20 +62,43 @@ class HashMap
     find_key_in_linked_list(key, @buckets[index].head)
   end
 
+  def linked_list_length(list)
+    current_node = list.head
+    count = 1
+    until current_node.next_node.nil?
+      current_node = current_node.next_node
+      count += 1
+    end
+    count
+  end
+
+  # Not sur if this will be useful, maybe better approach than below?
+  #
+  # def find_with_previous(key, list)
+  #   previous_node = list.head
+  #   current_node = list.head
+  #   until current_node.value[0] == key
+  #     previous_node = current_node
+  #     current_node = current_node.next_node
+  #     return if current_node.nil?
+  #   end
+  #   return [previous_node, current_node]
+  # end
+
   def remove_key_in_linked_list(key, list)
     current_node = list.head
     if current_node.value[0] == key
       list.head = current_node.next_node
-      return
+      return current_node.value[1]
     end
     until current_node.next_node.value[0] == key
       current_node = current_node.next_node
-      return nil if current_node.next_node == nil
+      return nil if current_node.next_node.nil?
     end
     value = current_node.next_node.value[1]
     current_node.next_node = current_node.next_node.next_node
 
-    return value
+    value
   end
 
   def find_key_in_linked_list(key, current_node)
@@ -86,7 +118,9 @@ map.set('john', 'new value')
 map.set('sara', 'old')
 map.set('nelson', 'new')
 
-map.remove('mark')
+p map.remove('sara')
+p map.length
+
 
 map.buckets.each_with_index do |bucket, index|
   p "In bucket #{index + 1} we have #{bucket}"
